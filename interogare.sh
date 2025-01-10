ea47137e1dba41e3f66fa910163d8b26da90445e
Danciu Valentin <valentindanciu02@gmail.com>
2025-01-10 11:35:47 +0000

    minor changes

--- interogare.sh
+++ interogare.sh
@@ -1,10 +1,57 @@
 #!/usr/bin/bash
 
-param = "$1"
+
 
 if [ -z "param" ]; then 
     echo "Nu ati introdus niciun parametru!"
     exit 1
 fi
+param="$1"
+if [ "$param" = "1" ]; then
+    for subdir in "../WorkDirectory"/*/; do
+   
+    subdirectory=$(basename "$subdir")
+    if [[ ! $subdirectory =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}$ ]]; then
 
-if [ "$param" = "1"]; then 
+        awk 'END {if ($0 ~ /(^|[[:space:]])installed($|[[:space:]])/) print $subdirectory;}' ../WorkDirectory/$subdirectory/$subdirectory
+
+    fi
+    done
+fi
+if [ "$param" = "2" ]; then
+    for subdir in "../WorkDirectory"/*/; do
+   
+    subdirectory=$(basename "$subdir")
+    if [[ ! $subdirectory =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}$ ]]; then
+
+        awk 'END {if ($0 ~ /(^|[[:space:]])removed($|[[:space:]])/) print $subdirectory;}' ../WorkDirectory/$subdirectory/$subdirectory
+
+    fi
+    done
+fi
+if [ "$param" = "3" ]; then
+    for subdir in "../WorkDirectory"/*/; do
+   
+    subdirectory=$(basename "$subdir")
+    if [[ ! $subdirectory =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}$ ]]; then
+
+        awk '{if ($0 ~ /(^|[[:space:]])half-installed($|[[:space:]])/) print $subdirectory;}' ../WorkDirectory/$subdirectory/$subdirectory
+
+    fi
+    done
+fi
+
+if [ "$param" = "4" ]; then
+    parampackage="$2"
+    awk 
+    
+    
+    
+    done
+fi
+
+        
+
+
+
+
